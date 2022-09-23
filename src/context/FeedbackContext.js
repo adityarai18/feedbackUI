bea56a1a31c619cf637b8b8009fa8ed0f1fa61a5
Aditya Rai <adityarai7240@gmail.com>
2022-09-23 17:21:48 +0530

    initial and hopefully final

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,92 @@
+import { createContext, useState, useEffect } from 'react';
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+
+    setFeedback(data);
+    setLoading(false);
+  };
+
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
+  };
+
+  const deleteFeedback = async (id) => {
+    if (window.confirm('Are you Sure ?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item: item,
+      edit: true,
+    });
+  };
+
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              ...data,
+            }
+          : item
+      )
+    );
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback: feedback,
+        feedbackEdit: feedbackEdit,
+        loading: loading,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum id ipsam consequatur suscipit eaque officia?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum id ipsam consequatur suscipit eaque officia?',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum id ipsam consequatur suscipit eaque officia?',
+  },
+];
+
+export default FeedbackData;
